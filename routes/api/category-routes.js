37208ef0f1ec2071c1cb20c41b11db252d272804
jmcdlungren <j.mcd.lungren@gmail.com>
2023-03-28 16:56:24 -0700

    completed models, working on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
   // be sure to include its associated Products
+    res.status(200).json(categoryData);
+  } catch (err) {
+  res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
