1434f2440c4b9e4e27df2d7e3aa96f27bddb19d0
jmcdlungren <j.mcd.lungren@gmail.com>
2023-03-30 12:03:39 -0700

    completed get routes in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,26 +5,22 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll(
+      {
+        include: [Product]
+      }
+    );
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // try {
-  //   const productData = await Product.findByPk(req.params.id, {
-  //     include: [{ model: Category, through: Product, as: 'category_products' }]
-  //   })
-
-  //   res.status(200).json(productData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id);
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
     if (!categoryData) {
       res.status(404).json({ message: 'No Category found with this id!' });
       return;
@@ -33,7 +29,6 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
 router.post('/', async (req, res) => {
